079b60b18a4824cfc481b66709129eac6f891aa8
HabaneroSpices <habanerospices@gmail.com>
2023-10-17 10:35:32 +0200

    Sync

--- scripts/setup-user.sh
+++ scripts/setup-user.sh
@@ -2,23 +2,24 @@
 #
 main (){
 
-info "Setting up user dotfiles"
+    info "Setting up user dotfiles"
 
-link ".aliases"
-link ".config/environment.d"
-link ".config/git/$(cut -d'-' -f1 /etc/hostname)" ".config/git/config"
-link ".config/git/common"
-link ".config/git/home"
-link ".config/git/ignore"
-link ".config/git/work"
-link ".zshrc"
-link ".p10k.zsh"
+    link ".aliases"
+    link ".config/environment.d"
+    #link ".config/git/$(cut -d'-' -f1 /etc/hostname)" ".config/git/config"
+    #link ".config/git/common"
+    #link ".config/git/home"
+    #link ".config/git/ignore"
+    #link ".config/git/work"
+    link ".gitconfig"
+    link ".gitconfig_alias"
+    link ".zshrc"
+    link ".p10k.zsh"
 
-#echo "Configure repo-local git settings"
-#git config user.email "habanerospices@gmail.com" && echo "user.mail->DONE"
-#git config user.name "HabaneroSpices" && echo "user.name->DONE"
-#git remote set-url origin "git@github.com:habanerospices/dotfiles_linux.git" && echo "set-url->DONE"
-
+    #echo "Configure repo-local git settings"
+    git config user.email "habanerospices@gmail.com" && log "Git email added to current repo"
+    git config user.name "HabaneroSpices" && log "Git username added to current repo"
+    #git remote set-url origin "git@github.com:habanerospices/dotfiles_linux.git" && echo "set-url->DONE"
 }
 
 ## Begin Default script
@@ -31,9 +32,9 @@ main
 
 ## If nothing was installed during execution exit 0
 if (( $UPTODATE == 0 ));then
-  success "Everything is up to date. Nothing to do." && exit 0
+    success "Everything is up to date. Nothing to do." && exit 0
 else
-  success "Successfully applied changes" && exit 0
+    success "Successfully applied changes" && exit 0
 fi
 
 ## End Default script
